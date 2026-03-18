5c7a71e312c073be3b7c3debff3876c2694e7625
deepakthakurela308-prog <deepakthakurela308@gmail.com>
2026-03-18 08:08:42 +0530

    Add Student class with constructor and display method
    This project is a simple Student Management System developed in Java. 
    It allows users to add and display student details such as ID, name, and marks using a menu-driven program.

---
+++ Student.java
@@ -0,0 +1,18 @@
+class Student {
+    int id;
+    String name;
+    int marks;
+
+    Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+        System.out.println("-------------------");
+    }
+}
